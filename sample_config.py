8d65730e1ddafae9dd788cd172d6dc4034cf1767
ValentinLHB <valentin.lhb@gmail.com>
2020-09-20 09:36:06 +0200

    added futures keys

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,9 @@
-binance = {
+binance_spot = {
     'public_key' : 'your_public_key',
-    'secret_key' : 'your_secret_key',
+    'secret_key' : 'your_private_key',
+}
+
+binance_futures = {
+    'public_key' : 'your_public_key',
+    'secret_key' : 'your_private_key',
 }
