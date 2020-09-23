d91d95a9c0870076cdee5fa48423f3d8867e9fe8
ValentinLHB <valentin.lhb@gmail.com>
2020-09-23 10:48:43 +0200

    added sample config file

---
+++ Bot/sample_config.py
@@ -0,0 +1,9 @@
+binance_spot = {
+    'public_key' : 'your_public_key',
+    'secret_key' : 'your_private_key',
+}
+
+binance_futures = {
+    'public_key' : 'your_public_key',
+    'secret_key' : 'your_private_key',
+}
