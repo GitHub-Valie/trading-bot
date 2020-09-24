4b9c19a83bf94a9f989919058b8e86872d8253ed
Valentin <valentin.lhb@gmail.com>
2020-09-24 15:14:09 +0200

    update params

--- Bot/config_bot.py
+++ Bot/config_bot.py
@@ -1,17 +1,26 @@
 bots = [ # Bots is a list of dictionaries that contain the respective currency pair of each trading bot. Each bot has its own trading pair
     {
         'pair' : 'BTCUSDT',
-        'fast_trend' : 7,
-        'slow_trend' : 15,
+        'fast_trend' : 61,
+        'slow_trend' : 18,
+        'leverage' : 5,
+        'percentage' : 12,
+        'precision' : 3,
     },
     {
         'pair' : 'BNBUSDT',
-        'fast_trend' : 9,
-        'slow_trend' : 20,
+        'fast_trend' : 72,
+        'slow_trend' : 22,
+        'leverage' : 1,
+        'percentage' : 20,
+        'precision' : 2,
     },
     {
         'pair' : 'ETHUSDT',
-        'fast_trend' : 4,
-        'slow_trend' : 10,
+        'fast_trend' : 68,
+        'slow_trend' : 21,
+        'leverage' : 5,
+        'percentage' : 12,
+        'precision' : 3,
     },
 ]
